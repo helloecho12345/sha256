0fbc166066a5a86d5067d2ad3e68d92a9924296d
Esther Cho <helloecho12345@gmail.com>
2022-04-15 21:43:58 +0100

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sha256
+
+To add the crypto-js library to your project, go the terminal and run the following command to install it using npm:
+
+npm install --save crypto-js
--- sha256.js
+++ sha256.js
@@ -1,3 +1,4 @@
+// imported the crypto-js JavaScript library and used its crypto-js/sha256 module to calculate the hash of each block. Since the module returns a number object, I used the toString() method to convert it into a string.
 const SHA256 = require('crypto-js/sha256');
 class CryptoBlock{
     constructor(index, timestamp, data, precedingHash=" "){
@@ -7,6 +8,7 @@ class CryptoBlock{
      this.precedingHash = precedingHash;   // precedingHash points to the hash of the preceding block in the blockchain, something important in maintaining the blockchain’s integrity.
      this.hash = this.computeHash();     
     }
+    // computeHash method to calculate the hash of the block based on its properties, as given in the data above
     computeHash(){
         return SHA256(this.index + this.precedingHash + this.timestamp + JSON.stringify(this.data)).toString();
     }   
