e6ba145be16ae808d7451fa0aace91e82d2eac34
Esther Cho <helloecho12345@gmail.com>
2022-04-15 21:48:50 +0100

    CryptoBlockchain class

--- sha256.js
+++ sha256.js
@@ -15,3 +15,21 @@ class CryptoBlock{
 }
 
 
+class CryptoBlockchain{
+  constructor(){
+      this.blockchain = [this.startGenesisBlock()];     
+  }
+  startGenesisBlock(){
+      return new CryptoBlock(0, "01/01/2020", "Initial Block in the Chain", "0");
+  }
+  obtainLatestBlock(){
+      return this.blockchain[this.blockchain.length - 1];
+  }
+  addNewBlock(newBlock){
+      newBlock.precedingHash = this.obtainLatestBlock().hash;
+      newBlock.hash = newBlock.computeHash();        
+      this.blockchain.push(newBlock);
+  }
+}
+
+
